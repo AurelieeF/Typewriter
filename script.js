a44d75770b026f1fb9f62301b2d9413c5a2c0e02
Aurélie Fidélia <aureliefidelia@gmail.com>
2026-09-04 15:21:59 -0400

    Making shift act like caplocks

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const paperText = document.getElementById("paper-text");
 let text = "";
+let shiftActive = false;
+
 
 const MAX_CHARACTERS = 200;
 
@@ -25,52 +27,82 @@ function getDataKey(key) {
     if (key.length === 1) {
         return key.toLowerCase();
     }
-    if (key === "CapsLock" || key === "Shift" || key === "Control" || key === "Alt" || key === "Meta") {
+    if (key === "CapsLock") {
         return "Shift";
     }
 
     return key;
 
 
+}
+window.addEventListener("keydown", (event) => {
+
+    const pressedKey = event.key;
+
+    const keyElement = document.querySelector(`.key[data-key="${getDataKey(pressedKey)}"]`);
+
+    if (keyElement) {
+        keyElement.classList.add("pressed");
     }
-    window.addEventListener("keydown", (event) => {
 
-        const pressedKey = event.key;
+    if(pressedKey === "CapsLock") {
+        shiftActive = event.getModifierState("CapsLock");
 
-        const keyElement = document.querySelector(`.key[data-key="${getDataKey(pressedKey)}"]`);
+        if(shiftActive) {
+            keyElement.classList.add("active");
+        } else {
+            keyElement.classList.remove("active");
+        }
+        return;
+    }
+    handleKey(pressedKey);
 
-        if (keyElement) {
-            keyElement.classList.add("pressed");
+});
+
+window.addEventListener("keyup", (event) => {
+
+    const releasedKey = event.key;
+
+    const keyElement = document.querySelector(`.key[data-key="${getDataKey(releasedKey)}"]`);
+
+    if (keyElement) {
+        keyElement.classList.remove("pressed");
+    }
+
+});
+
+
+const keys = document.querySelectorAll(".key");
+
+keys.forEach((key) => {
+    key.addEventListener("mousedown", () => {
+        key.classList.add("pressed");
+
+        const clickedKey = key.dataset.key;
+
+        if (clickedKey === "Shift") {
+            shiftActive = !shiftActive;
+
+            if(shiftActive) {
+                key.classList.add("active");
+
+            } else {
+                key.classList.remove("active");
+            }
+            
+            return;
         }
 
-        handleKey(pressedKey);
-
-
-
-    });
-
-    window.addEventListener("keyup", (event) => {
-
-        const releasedKey = event.key;
-
-        const keyElement = document.querySelector(`.key[data-key="${getDataKey(releasedKey)}"]`);
-
-        if (keyElement) {
-            keyElement.classList.remove("pressed");
+        if (shiftActive && clickedKey.length === 1) {
+            handleKey(clickedKey.toUpperCase());
+        }
+        else {
+            handleKey(clickedKey);
         }
 
     });
-
-
-    const keys = document.querySelectorAll(".key");
-
-    keys.forEach((key) => {
-        key.addEventListener("mousedown", () => {
-            key.classList.add("pressed");
-            handleKey(key.dataset.key);
-        });
-        key.addEventListener("mouseup", () => {
-            key.classList.remove("pressed");
-        });
+    key.addEventListener("mouseup", () => {
+        key.classList.remove("pressed");
     });
+});
 
