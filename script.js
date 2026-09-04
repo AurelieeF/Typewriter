4cd745cb49267d7672594c6362729b5e5674d8fb
Aurélie Fidélia <aureliefidelia@gmail.com>
2026-09-04 14:13:25 -0400

    Touche Enter and Backspace

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
-const paperText= document.getElementById("paper-text"); 
-let text="";
+const paperText = document.getElementById("paper-text");
+let text = "";
 
-const MAX_CHARACTERS=200;
+const MAX_CHARACTERS = 200;
 window.addEventListener("keydown", (event) => {
-    
-    const pressedKey=event.key;
+
+    const pressedKey = event.key;
 
     console.log(pressedKey);
     if (pressedKey.length === 1 && text.length < MAX_CHARACTERS) {
         text += pressedKey;
-        paperText.textContent = text;
     }
+
+    else if (pressedKey === "Backspace") {
+        text = text.slice(0, -1);
+    }
+
+    else if (pressedKey === "Enter" && text.length < MAX_CHARACTERS) {
+        text += "\n";
+    }
+
+    paperText.textContent = text;
+
 });
 
 
-    
