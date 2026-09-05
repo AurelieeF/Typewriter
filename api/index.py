3dc950110e0f12513f987b73a0d5d5d420db4e32
Aurélie Fidélia <aureliefidelia@gmail.com>
2026-09-05 11:03:22 -0400

    update index.py + test de flask vers Neon

--- api/index.py
+++ api/index.py
@@ -1,8 +1,11 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+import os
+import psycopg
 
 app = Flask(__name__)
 CORS(app)
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
 
 @app.route("/api/test", methods=["GET"])
@@ -22,4 +25,17 @@ def save_note():
     return jsonify({
         "message": "Note received!",
         "note": note
+    })
+
+@app.route("/api/db-test", methods=["GET"])
+def db_test():
+
+    with psycopg.connect(DATABASE_URL) as connection:
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT 1")
+            result = cursor.fetchone()
+
+    return jsonify({
+        "message": "Database connected!",
+        "result": result[0]
     })
