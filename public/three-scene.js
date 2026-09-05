c5a31b7987cff32324c23dfd7359054c7906b4e4
Aurélie Fidélia <aureliefidelia@gmail.com>
2026-09-05 18:38:39 -0400

    change orientation

--- public/three-scene.js
+++ public/three-scene.js
@@ -126,7 +126,7 @@ loader.load(
         camera.position.set(
             0,
             maxDimension * 1.4,
-            maxDimension * 1.8
+            maxDimension * -1.8
         );
 
         camera.lookAt(
