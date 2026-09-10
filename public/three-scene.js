583e731975f2053407388d72f1cf68214380874d
Aurélie Fidélia <aureliefidelia@gmail.com>
2026-09-09 21:10:06 -0400

    0 effets

--- public/three-scene.js
+++ public/three-scene.js
@@ -188,7 +188,7 @@ loader.load(
         camera.position.set(
             0,
             maxDimension * 1.05,
-            maxDimension * 1.65
+            maxDimension * 1.90
         );
 
         camera.lookAt(
